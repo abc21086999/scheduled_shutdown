0b64c30a76f33dab793826854b7cca89a2f51534
abc21086999 <abc21086999@gmail.com>
2024-06-13 00:14:40 +0800

    copy paste some code from the internet

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+import tkinter as tk
+window = tk.Tk()
+window.title('定時關機程式')
+window.geometry('380x400')
+window.iconbitmap("C:\Program Files\Gamania\MapleStory\MapleStory.ico")
+window.resizable(False, False)
+window.mainloop()
